1a392d86305609db25ce30943e894722e5f4e2c6
KaioJesus <kaioramos990@hotmail.com>
2017-04-28 22:15:17 -0300

    revisão de struct 28/04

--- Exercicios/ConsoleApplication1/Program.cs
+++ Exercicios/ConsoleApplication1/Program.cs
@@ -8,64 +8,70 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        struct veiculo
+        {
+            public string modelo;
+            public double km, potencia;
+        }
+
         static void Main(string[] args)
         {
             int N;
-            string modelo, valor;
-            double km, pot;
+            string valor;
 
             Console.WriteLine("Informe a quantidade de carros no galpão");
             N = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < N; i++)
             {
-                Console.WriteLine("Informe o modelo do carro");
-                modelo = Console.ReadLine();
+                veiculo automovel;
+                Console.WriteLine("Informe o modelo");
+                automovel.modelo = Console.ReadLine();
 
-                Console.WriteLine("Informe a quilometragem do carro");
-                km = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe sua quilometragem");
+                automovel.km = Double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Informe a potência do motor do carro");
-                pot = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a potência do motor");
+                automovel.potencia = Double.Parse(Console.ReadLine());
 
-                valor = Carro(modelo, km, pot);
-
+                valor = Carro(automovel);
                 Console.WriteLine("{0}", valor);
             }
         }
-            string Carro(string mode, double kmt, double pote)
+        string Carro(veiculo parametro)
+        {
+            string brabo, tiponsv, tipopfp;
+
+            if (parametro.km <= 5000)
             {
-                string brabo, tiponsv, tipopfp;
-
-                if (kmt <= 5000)
-                {
-                    tiponsv = "novo";
-                }
-                else if (kmt > 5000 && kmt <= 30000)
-                {
-                    tiponsv = "seminovo";
-                }
-                else
-                {
-                    tiponsv = "velho";
-                }
-
-                if (pote > 200)
-                {
-                    tipopfp = "potente";
-                }
-                else if (pote >= 120 && pote <= 200)
-                {
-                    tipopfp = "forte";
-                }
-                else
-                {
-                    tipopfp = "popular";
-                }
-
-                brabo = mode + " - " + tipod + " - " + tiponsv;
-                return brabo;
-            
+                tiponsv = "novo";
             }
+            else if (parametro.km > 5000 && parametro.km <= 30000)
+            {
+                tiponsv = "seminovo";
+            }
+            else
+            {
+                tiponsv = "velho";
+            }
+
+            if (parametro.potencia > 200)
+            {
+                tipopfp = "potente";
+            }
+            else if (parametro.potencia >= 120 && parametro.potencia <= 200)
+            {
+                tipopfp = "forte";
+            }
+            else
+            {
+                tipopfp = "popular";
+            }
+
+            brabo = parametro.modelo + " - " + tipopfp + " - " + tiponsv;
+            return brabo;
+        }
+
+
     }
 }
