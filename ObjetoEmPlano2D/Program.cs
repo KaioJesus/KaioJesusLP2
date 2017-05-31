b0f56396d49f497fc24b75d43445bedc696c0436
Kaio Jesus <kaioramos990@hotmail.com>
2017-05-31 16:03:51 -0300

    Exercicio 31/05/2017

---
+++ ObjetoEmPlano2D/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetoEmPlano2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 600, L = 800;
+
+            Console.Write("Digite X inicial: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite Y incial: ");
+            int y = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                Objeto2D obj = new Objeto2D();
+                obj.x = x;
+                obj.y = y;
+                Console.Write("Digite comando");
+                ConsoleKey comando = Console.ReadKey().Key;
+
+                if (comando == ConsoleKey.RightArrow)
+                {
+                    obj.AndarParaADireita();
+                    Console.WriteLine(obj.Coordenadas());
+                }
+            }
+        }
+    }
+}
