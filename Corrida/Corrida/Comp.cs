d2f60f6898496ff3a3c38ec8025b134a1bf7a980
Guilherme da Silva <kojofx@gmail.com>
2017-06-07 15:59:38 -0300

    07/06 Incompleto

---
+++ Corrida/Corrida/Comp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corrida
+{
+    class Comp
+    {                 
+        public string nome;
+        public double prim;
+        public double seg;
+        public double terc;
+        
+    }
+}
