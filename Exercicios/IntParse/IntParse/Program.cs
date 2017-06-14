0c4f79d8fbe79297185f32891fd5bf143ceb880f
KaioJesus <kaioramos990@hotmail.com>
2017-06-13 22:42:00 -0300

    Meu Int.Parse

---
+++ Exercicios/IntParse/IntParse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número");
+            string numStr = Console.ReadLine();
+
+            Console.WriteLine(IntParse(numStr));
+        }
+
+        public static int IntParse(string numroIng)
+        {
+            int multi = 1;
+            int numero = 0;
+
+            for (int i = numroIng.Length - 1; i >= 0; i--)
+            {
+                numero = numero + (numroIng[i] - '0') * multi;
+                multi = multi * 10;
+            }
+            return numero;
+        }
+    }
+}
