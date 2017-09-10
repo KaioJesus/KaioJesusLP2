e0fd7b0f460e718ebe7d09b3c5d66d2c006ceedd
Kaio Jesus Ramos <kaioramos990@hotmail.com>
2017-09-10 18:26:10 -0300

    30/06 incompleto

---
+++ 2C/07-ConversorMedidas/07-ConversorMedidas/ConversorMassa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ConversorMedidas
+{
+    static class ConversorMassa
+    {
+        public double kg;
+        public double g;
+        public double t;
+        public double lb;
+
+        public double kgparag()
+        {
+            double x = kg * 1000;
+            return x;
+        }
+        public double kgparalb()
+        {
+            double y = 
+        }
+    }
+}
