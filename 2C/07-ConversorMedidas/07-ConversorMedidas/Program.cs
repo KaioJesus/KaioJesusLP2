d8e92e671222f4bbf4f58a09276d81916f24b191
Kaio Jesus Ramos <kaioramos990@hotmail.com>
2017-09-21 17:39:48 -0300

    Exercício de conversão de medidas completo (estático) 21/09

--- 2C/07-ConversorMedidas/07-ConversorMedidas/Program.cs
+++ 2C/07-ConversorMedidas/07-ConversorMedidas/Program.cs
@@ -10,6 +10,28 @@ namespace _07_ConversorMedidas
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ConversordeMedidas.GParaKg(1));
+            Console.WriteLine(ConversordeMedidas.KgparaG(1));
+            Console.WriteLine(ConversordeMedidas.KgparaT(1));
+            Console.WriteLine(ConversordeMedidas.TparaKg(1));
+            Console.WriteLine(ConversordeMedidas.KgparaLBS(1));
+            Console.WriteLine(ConversordeMedidas.LBSparaKG(1));
+            Console.WriteLine(ConversordeMedidas.TfparaTc(1));
+            Console.WriteLine(ConversordeMedidas.TcparaTf(1));
+            Console.WriteLine(ConversordeMedidas.TfparaTk(1));
+            Console.WriteLine(ConversordeMedidas.TkparaTf(1));
+            Console.WriteLine(ConversordeMedidas.TcparaTk(1));
+            Console.WriteLine(ConversordeMedidas.TkparaTc(1));
+            Console.WriteLine(ConversordeMedidas.MparaKm(1));
+            Console.WriteLine(ConversordeMedidas.KmparaM(1));
+            Console.WriteLine(ConversordeMedidas.KmparaMilha(1));
+            Console.WriteLine(ConversordeMedidas.MilhaparaKm(1));
+            Console.WriteLine(ConversordeMedidas.MparaPes(1));
+            Console.WriteLine(ConversordeMedidas.PesparaM(1));
+            Console.WriteLine(ConversordeMedidas.MparaPol(1));
+            Console.WriteLine(ConversordeMedidas.PolparaM(1));
+            Console.WriteLine(ConversordeMedidas.PolparaPes(1));
+            Console.WriteLine(ConversordeMedidas.PesparaPol(1));
         }
     }
 }
